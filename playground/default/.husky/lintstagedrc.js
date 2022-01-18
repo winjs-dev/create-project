76e4244564005939fa14e1dde067f1817fd505d8
cklwblove <lwbhtml@163.com>
2022-01-18 10:17:59 +0800

    feat(zx): 命令行工具更改为 zx

---
+++ playground/default/.husky/lintstagedrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // 对指定格式文件 在提交的时候执行相应的修复命令
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write --parser json'],
+  'package.json': ['prettier --write'],
+  '*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write', 'git add .'],
+  '*.{scss,less,styl,css,html}': ['stylelint --fix', 'prettier --write', 'git add .'],
+  '*.md': ['prettier --write']
+};
---
+++ scripts/prepublish.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env zx
+import 'zx/globals'
+
+await $`pnpm build`
+await $`pnpm snapshot`
+
+let { version } = JSON.parse(await fs.readFile('./package.json'))
+
+const playgroundDir = path.resolve(__dirname, '../playground/')
+cd(playgroundDir)
+
+await $`git add -A .`
+try {
+  await $`git commit -m "version ${version} snapshot"`
+} catch (e) {
+  if (!e.stdout.includes('nothing to commit')) {
+    throw e
+  }
+}
+
+await $`git tag -m "v${version}" v${version}`
+await $`git push --follow-tags`
