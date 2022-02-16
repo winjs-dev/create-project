120e349d30638386adb7079550d17b6a0d643e8d
cklwblove <lwbhtml@163.com>
2022-02-16 13:14:09 +0800

    version 1.3.4 snapshot

---
+++ playground/mirror-source/v2/babel.config.js
@@ -0,0 +1,23 @@
+const plugins = [
+  [
+    'import',
+    {
+      libraryName: '@winner-fed/win-ui',
+      libraryDirectory: 'es',
+      style: true
+    },
+    '@winner-fed/win-ui'
+  ]
+];
+
+module.exports = {
+  presets: [
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        useBuiltIns: 'entry'
+      }
+    ]
+  ],
+  plugins
+};
