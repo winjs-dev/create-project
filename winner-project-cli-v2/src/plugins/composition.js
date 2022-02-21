dc18a97791195f7cabad476277caf8c4b77440e6
cklwblove <lwbhtml@163.com>
2022-02-21 15:55:34 +0800

    refactor(3.x): vue3.x 不兼容 ie11

--- winner-project-cli-v2/src/plugins/composition.js
+++
@@ -1,6 +0,0 @@
-// support composition-api
-// https://www.npmjs.com/package/@vue/composition-api
-import Vue from 'vue';
-import VueCompositionAPI from '@vue/composition-api';
-
-Vue.use(VueCompositionAPI);
