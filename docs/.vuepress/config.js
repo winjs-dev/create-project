6323fbc27526eac104815627d7cd846236fc0e29
cklwblove <lwbhtml@163.com>
2022-03-10 20:58:35 +0800

    feat(logo): 添加logo等素材

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,42 @@ module.exports = {
   description: '🛠️ 鲸腾FE工程开发的标准工具',
   base: '/create-project/',
   head: [
-    ['meta', { name: 'application-name', content: 'VuePress' }],
-    ['meta', { name: 'apple-mobile-web-app-title', content: 'VuePress' }],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: `/images/icons/favicon-16x16.png`,
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: `/images/icons/favicon-32x32.png`,
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'application-name', content: 'create-project' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'create-project' }],
     [
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/images/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
+      },
     ],
     ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
@@ -47,6 +78,9 @@ module.exports = {
     ],
   ],
   themeConfig: {
+    logo: '/images/hero.png',
+    docsDir: 'docs',
+
     navbar: [
       {
         text: '指南',
