93a0d60deeb4be1c7eef6da2a6b94b24328ffe4c
cklwblove <lwbhtml@163.com>
2023-05-25 13:45:31 +0800

    deploy

---
+++ assets/index.html.de1bec93.js
@@ -0,0 +1,15 @@
+import{_ as o,o as p,a as t,e,w as a,F as i,b as s,f as n,r as l}from"./app.6f6cde24.js";const u={},b=s("h3",{id:"\u50CF\u6570-1-2-3-\u4E00\u6837\u5BB9\u6613",tabindex:"-1"},[s("a",{class:"header-anchor",href:"#\u50CF\u6570-1-2-3-\u4E00\u6837\u5BB9\u6613","aria-hidden":"true"},"#"),n(" \u50CF\u6570 1, 2, 3 \u4E00\u6837\u5BB9\u6613")],-1),m=s("div",{class:"language-bash ext-sh line-numbers-mode"},[s("pre",{class:"shiki",style:{"background-color":"#1E1E1E"}},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5728\u4F60\u7684\u9879\u76EE\u4E2D\u5B89\u88C5")]),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npm init @winner-fed/project@latest my-project")]),n(`
+`),s("span",{class:"line"}),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u8FDB\u5165\u5DE5\u7A0B")]),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#DCDCAA"}},"cd"),s("span",{style:{color:"#D4D4D4"}}," my-project")]),n(`
+`),s("span",{class:"line"}),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5B89\u88C5\u4F9D\u8D56")]),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npm run bootstrap")]),n(`
+`),s("span",{class:"line"}),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u5F00\u53D1\u8C03\u8BD5")]),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npm run serve")]),n(`
+`),s("span",{class:"line"}),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#6A9955"}},"# \u6784\u5EFA\u6253\u5305")]),n(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#D4D4D4"}},"npm run build")]),n(`
+`),s("span",{class:"line"})])]),s("div",{class:"line-numbers","aria-hidden":"true"},[s("span",{class:"line-number"},"1"),s("br"),s("span",{class:"line-number"},"2"),s("br"),s("span",{class:"line-number"},"3"),s("br"),s("span",{class:"line-number"},"4"),s("br"),s("span",{class:"line-number"},"5"),s("br"),s("span",{class:"line-number"},"6"),s("br"),s("span",{class:"line-number"},"7"),s("br"),s("span",{class:"line-number"},"8"),s("br"),s("span",{class:"line-number"},"9"),s("br"),s("span",{class:"line-number"},"10"),s("br"),s("span",{class:"line-number"},"11"),s("br"),s("span",{class:"line-number"},"12"),s("br"),s("span",{class:"line-number"},"13"),s("br"),s("span",{class:"line-number"},"14"),s("br")])],-1);function d(_,D){const r=l("CodeGroupItem"),c=l("CodeGroup");return p(),t(i,null,[b,e(c,null,{default:a(()=>[e(r,{title:"NPM"},{default:a(()=>[m]),_:1})]),_:1})],64)}var y=o(u,[["render",d],["__file","index.html.vue"]]);export{y as default};
